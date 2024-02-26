1813fd01fa3f14a6549f71c00e056a8be96ea7d0
Ashesh Singh <ask@mrsinh.com>
2024-02-26 07:46:22 -0600

    Add Webex

--- channels.js
+++ channels.js
@@ -85,6 +85,10 @@ module.exports = [
         slug: 'rocket-chat', name: 'Rocket Chat',
         maintainers: ['antonkomarev', 'FunfareNico'],
       },
+      {
+        slug: 'webex', name: 'Webex',
+        maintainers: ['askmrsinh'],
+      },
     ],
   },
   {
